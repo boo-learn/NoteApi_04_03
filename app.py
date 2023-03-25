f8d96503fdd62b2ce4b7e6e0feca06fe6b4f8bd7
boo <eyurchenko@specialist.ru>
2023-03-25 18:51:28 +0700

    new resource: Tag

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from api import app, docs
 from config import Config
-from api.handlers import auth, note, user
+from api.handlers import auth, note, user, tag
 
 # CRUD
 
@@ -8,12 +8,21 @@ from api.handlers import auth, note, user
 # Read --> GET
 # Update --> PUT
 # Delete --> DELETE
+
+# USERS
 docs.register(user.get_user_by_id)
 docs.register(user.create_user)
 docs.register(user.get_users)
+
+# NOTES
 docs.register(note.get_note_by_id)
 docs.register(note.get_notes)
 docs.register(note.create_note)
+
+# TAGS
+docs.register(tag.get_tag_by_id)
+docs.register(tag.get_tags)
+docs.register(tag.create_tag)
 
 if __name__ == '__main__':
     app.run(debug=Config.DEBUG, port=Config.PORT)
